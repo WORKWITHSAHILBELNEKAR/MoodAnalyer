9eb214192092ae707b8d691c4467b26e7efb5fb8
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-09-15 00:56:06 +0530

    it@Test

--- MoodAnalyser/src/test/java/com/bl/java/moodanalyer/MoodAnalyerTest.java
+++ MoodAnalyser/src/test/java/com/bl/java/moodanalyer/MoodAnalyerTest.java
@@ -9,9 +9,9 @@ class MoodAnalyerTest {
     @Test
     public void givenMessage_WhenProper_ShouldReturnSad(){
         MoodAnalyer moodAnalyser = new MoodAnalyer();
-        String result = moodAnalyser.analyseMood("I am In Sad mood.");
+        String result = moodAnalyser.analyseMood("I am In Happy mood.");
         System.out.println(result);
-        assertEquals("Sad" , result);
+        assertEquals("Happy" , result);
     }
 
 
