b51efa57cb1f9764c19c53764108cc40305e28b9
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-09-15 10:50:36 +0530

    repeat the test cases

--- MoodAnalyser/src/test/java/com/bl/java/moodanalyer/MoodAnalyerTest.java
+++ MoodAnalyser/src/test/java/com/bl/java/moodanalyer/MoodAnalyerTest.java
@@ -1,24 +1,17 @@
 package com.bl.java.moodanalyer;
 
-import org.testng.annotations.Test;
+
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MoodAnalyserTest {
+public class MoodAnalyerTest {
 
     @Test
-
     public void givenMessage_WhenProper_ShouldReturnSad(){
         MoodAnalyer moodAnalyser = new MoodAnalyer("I Am In Sad mood.");
         String result = moodAnalyser.analyseMood();
         System.out.println(result);
         assertEquals("SAD" , result);
-    }
-    @Test
-    public void givenMessage_WhenProper_ShouldReturnHappy(){
-        MoodAnalyer moodAnalyser = new MoodAnalyer("I Am In a Happy mood.");
-        String result = moodAnalyser.analyseMood();
-        System.out.println(result);
-        assertEquals("HAPPY" , result);
     }
 }
